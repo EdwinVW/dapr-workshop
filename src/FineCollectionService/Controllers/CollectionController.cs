3d8cc01564defcb150c808717f27f4916620bf14
EdwinVW <edwinvw@gmail.com>
2021-02-27 16:07:03 +0100

    Remove unnecessary using

--- src/FineCollectionService/Controllers/CollectionController.cs
+++ src/FineCollectionService/Controllers/CollectionController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using FineCollectionService.DomainServices;
 using FineCollectionService.Helpers;
